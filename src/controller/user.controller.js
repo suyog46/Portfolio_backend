750ba1ba423fad45e93fb6f645c173d40f299d4f
suyog46 <suyog.211446@ncit.edu.np>
2025-05-10 11:25:55 +0545

    catcha added

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,19 +3,32 @@ import { ApiError } from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import userModel from "../model/user.model.js";
 import { sendFeedbackEmail } from "../utils/sendMail.js";
-// import { sendFeedbackEmail } from "../utils/sendMail"; 
+import axios from "axios";
+
 
 
 export const handleuser=asyncHandler(async(req,res)=>{
 
     console.log("data is",req.body);
-    const {userName,userEmail,userFeedback}=req.body;
+    const {userName,userEmail,userFeedback,token}=req.body;
 
 
 
-if(!userName| !userEmail |!userFeedback){
-    throw new ApiError(404,"username or email or Feedback cannot be empty")
+if(!userName|| !userEmail ||!userFeedback || !token){
+    throw new ApiError(404,"Missing required fields or CAPTCHA token")
 }
+
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY; // Add this to your .env file
+  const googleResponse = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`
+  );
+
+  const { success, score, action } = googleResponse.data;
+
+  if (!success || score < 0.5) {
+    throw new ApiError(403, "Failed CAPTCHA verification");
+  }
+
 const user=await userModel.create({
     userName:userName,
     email:userEmail,
